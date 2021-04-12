aff17f01d76cf92d0889ed1b801862ef35186473
Harrf Akbar <harrf_akbar@yahoo.com>
2021-04-12 16:53:02 -0700

    added create post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::PostsController < ApplicationController
+
+    skip_before_action :authorized
+
+
+    def index 
+        posts = Post.all 
+        render json: posts 
+    end
+
+    def create 
+        post = Post.create(post_params)
+        render json: post 
+    end
+
+    private 
+
+    def post_params
+        params.permit( :content, :user_id )
+    end
 end
