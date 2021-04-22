92ad61c8c052d3f78c340071433242deae372ca1
Harrf Akbar <harrf_akbar@yahoo.com>
2021-04-22 13:48:50 -0700

    minor changes

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::CommentsController < ApplicationController
+
+    def index 
+        comments = Comments.all
+        render json: comments 
+    end
+
+    def create 
+        comment = Comments.create(comment_params)
+        render json: comment
+    end
+
+    private 
+
+    def comment_params 
+        params.permit(:comment, :user_id, :post_id)
+    end
 end
